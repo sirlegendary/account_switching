0dc6254434fd50e53047444aebf21ff0d4c1f316
Wale Salami <walesalami@Wales-MBP-2.broadband>
2020-04-20 19:12:09 +0100

    Admin page customisation complete

--- account_switching/settings.py
+++ account_switching/settings.py
@@ -31,13 +31,13 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'my_app.apps.MyAppConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'my_app.apps.MyAppConfig',
 ]
 
 MIDDLEWARE = [
--- my_app/admin.py
+++ my_app/admin.py
@@ -2,4 +2,12 @@ from django.contrib import admin
 from .models import Bank, Offer
 
 admin.site.register(Bank)
-admin.site.register(Offer)
+class OfferAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None,               {'fields': ['bank']}),
+        ('Offer details',    {'fields': ['switch_bonus','referral','min_mthly_pay_in','req_dd','extra_perks']}),
+        ('URL to offer',     {'fields': ['offer_url']}),
+        ('Date information', {'fields': ['start_date','end_date']}),
+    ]
+
+admin.site.register(Offer, OfferAdmin)
