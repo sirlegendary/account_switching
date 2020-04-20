1524d7e1f044df86cfaf145ca0179fae6a19db15
Wale Salami <walesalami@Wales-MBP-2.broadband>
2020-04-20 22:37:14 +0100

    trying to push to heroku

--- account_switching/settings.py
+++ account_switching/settings.py
@@ -121,8 +121,8 @@ USE_TZ = True
 
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
+# STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
 
-# STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
 django_heroku.settings(locals())
