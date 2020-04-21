d0f8c29745eb1685f668bdaedfbf783dab551524
Wale Salami <walesalami@Wales-MBP-2.broadband>
2020-04-22 00:08:36 +0100

    updated models with new fields and updated admin page to show new fields

--- my_app/admin.py
+++ my_app/admin.py
@@ -4,10 +4,15 @@ from .models import Bank, Offer
 admin.site.register(Bank)
 class OfferAdmin(admin.ModelAdmin):
     fieldsets = [
-        (None,               {'fields': ['bank']}),
-        ('Offer details',    {'fields': ['switch_bonus','referral','min_mthly_pay_in','req_dd','extra_perks']}),
+        (None,               {'fields': ['bank','offer_title']}),
+        ('Offer details',    {'fields': ['switch_bonus','referral','min_mthly_pay_in','req_dd','monthly_bonus','monthly_bonus_cmt','monthly_interest']}),
+        ('Fees',             {'fields': ['fees','fees_comment']}),
+        ('Extra Perks',      {'fields': ['extra_perks']}),
         ('URL to offer',     {'fields': ['offer_url']}),
+        ('Instructions',     {'fields': ['instructions']}),
+        ('Terms',            {'fields': ['terms']}),
         ('Date information', {'fields': ['start_date','end_date']}),
     ]
 
 admin.site.register(Offer, OfferAdmin)
+
--- my_app/models.py
+++ my_app/models.py
@@ -4,7 +4,7 @@ from django.utils import timezone
 
 class Bank(models.Model):
     bank_name = models.CharField(max_length=200)
-    bank_url  = models.CharField(max_length=200)
+    bank_url  = models.CharField(max_length=250)
 
     def __str__(self):
         return self.bank_name
@@ -12,14 +12,23 @@ class Bank(models.Model):
 
 class Offer(models.Model):
     bank                = models.ForeignKey(Bank, on_delete=models.CASCADE)
-    switch_bonus        = models.IntegerField(default=0)
+    offer_title         = models.CharField(max_length=200, blank = True)
+    switch_bonus        = models.DecimalField(default=0, max_digits=19, decimal_places=2)
     referral            = models.BooleanField(default=False)
-    min_mthly_pay_in    = models.IntegerField(default=0)
+    min_mthly_pay_in    = models.DecimalField(default=0, max_digits=19, decimal_places=2)
     req_dd              = models.IntegerField(default=0)
-    extra_perks         = models.CharField(max_length=200)
+    extra_perks         = models.CharField(max_length=500)
+    fees                = models.DecimalField(default=0, max_digits=19, decimal_places=2)
+    fees_comment        = models.CharField(max_length=250, blank = True)
+    monthly_bonus       = models.DecimalField(default=0, max_digits=19, decimal_places=2)
+    monthly_bonus_cmt   = models.CharField(max_length=250, blank = True)
+    instructions        = models.TextField(blank = True) 
+    monthly_interest    = models.DecimalField(default=0, max_digits=5, decimal_places=2)
+    terms               = models.TextField(blank = True) 
     offer_url           = models.CharField(max_length=250)
     start_date          = models.DateTimeField("Offer start date")
     end_date            = models.DateTimeField("Offer end date")
 
     def __str__(self):
        return self.bank.bank_name
+
